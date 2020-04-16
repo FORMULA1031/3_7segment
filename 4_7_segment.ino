5447afa44f4859fd967f2028f279227a4f00745b
FORMULA1031 <56063659+FORMULA1031@users.noreply.github.com>
2020-04-17 00:31:18 +0900

    0~999までカウントする

---
+++ 4_7_segment.ino
@@ -0,0 +1,68 @@
+// LEDをオン・オフする際の出力
+#define DIGIT_ON HIGH
+#define DIGIT_OFF LOW
+#define SEGMENT_ON LOW
+#define SEGMENT_OFF HIGH
+
+const int digitPins[] = {11, 10, 9};                                        // ディジット(コモン)に接続するArduinoのピン
+const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8};                             // セグメントピンに接続するArduinoのピン
+const int numberOfDigitPins = sizeof(digitPins) / sizeof(digitPins[0]);        // ディジットの数
+const int numberOfSegmentPins = sizeof(segmentPins) / sizeof(segmentPins[0]);  // セグメントの数
+
+// 数字と表示させるセグメントの関係
+const int digits[] = {
+    0b00111111,  // 0
+    0b00000110,  // 1
+    0b01011011,  // 2
+    0b01001111,  // 3
+    0b01100110,  // 4
+    0b01101101,  // 5
+    0b01111101,  // 6
+    0b00100111,  // 7
+    0b01111111,  // 8
+    0b01101111,  // 9
+};
+
+// 数字を表示する
+void displayNumber(int n) {
+  // digits[n]の各ビットを調べて対応するセグメントを点灯・消灯する
+  for (int i = 0; i < numberOfSegmentPins; i++) {
+    digitalWrite(segmentPins[i], digits[n] & (1 << i) ? SEGMENT_ON : SEGMENT_OFF);
+  }
+}
+
+// セグメントをすべてオフにする
+void clearSegments() {
+  for (int j = 0; j < numberOfSegmentPins; j++) {
+    digitalWrite(segmentPins[j], SEGMENT_OFF);
+  }
+}
+
+// 4桁の数字を表示する
+void displayNumbers(int n) {
+  for (int i = 0; i < numberOfDigitPins; i++) {  // 右の桁からディジットを選択する
+    digitalWrite(digitPins[i], DIGIT_ON);        // ディジットをオンにする
+    displayNumber(n % 10);                       // 10で割った余りを求めて、1の位を求め、表示する
+    delay(10);
+    clearSegments();                        // セグメントをすべてオフにする
+    digitalWrite(digitPins[i], DIGIT_OFF);  // ディジットをオフにする
+    n /= 10;                                // 10で割り、次に表示する数字を、1の位に移す
+  }
+}
+
+// setup()　は，最初に一度だけ実行される
+void setup() {
+  for (int i = 0; i < numberOfDigitPins; i++) {
+    pinMode(digitPins[i], OUTPUT);  // digitPinsを出力モードに設定する
+    digitalWrite(digitPins[i], DIGIT_OFF);
+  }
+  for (int i = 0; i < numberOfSegmentPins; i++) {
+    pinMode(segmentPins[i], OUTPUT);  // segmentPinsを出力モードに設定する
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 1000; i++) {  // 0から999までを表示する。
+    displayNumbers(i);
+  }
+}
